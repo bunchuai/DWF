8e37e008e80fbc791094281dbc8514be544c4192
Passakorn Panphromthumrong <passakorn.bom@gmail.com>
2016-06-24 13:20:37 +0700

    Contact V1

---
+++ src/www.dwf.go.th/Controllers/Contact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace www.dwf.go.th.Controllers
+{
+    public class Contact : Controller
+    {
+        // GET: /<controller>/
+        // ส่วนกลาง
+        public IActionResult Center()
+        {
+            return View();
+        }
+
+        // ส่วนภูมิภาค
+        public IActionResult Region()
+        {
+            return View();
+        }
+
+        // ถาม-ตอบ
+        public IActionResult QA()
+        {
+            return View();
+        }
+
+        // คำถามที่พบบ่อย
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
