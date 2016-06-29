a80f3727e77df541fb5ab052419f569999c69e77
Passakorn Panphromthumrong <passakorn.bom@gmail.com>
2016-06-29 13:16:13 +0700

    [Bom] 29-06-2559 13:16

---
+++ src/www.dwf.go.th/Controllers/News.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace www.dwf.go.th.Controllers
+{
+    public class News : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Public_News()
+        {
+            return View("Public_News/Public_News");
+        }
+    }
+}
